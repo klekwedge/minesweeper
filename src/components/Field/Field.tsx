f5c509bdb492723b0851c24a765955a22b8d514f
Andrey <antonov.andrey.10101@gmail.com>
2023-02-27 16:22:30 +0300

    feat: add masks for cells

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,29 +1,73 @@
-import { Flex, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Flex, Box, Button } from '@chakra-ui/react';
 import { v4 as uuidv4 } from 'uuid';
-import imageSrc from '../../../public/minesweeper-sprites.png';
+import sprite from '/public/minesweeper-sprites.png';
 import useCreateField from '../../hooks/useCreateField';
 
+// type cellType = 'hide' | 'show' | 'flag';
+
+enum MaskCell {
+  hidden,
+  show,
+  flag,
+  question,
+}
+
+const MaskCellType = {
+  [MaskCell.hidden]: '0px -50px',
+  [MaskCell.show]: '-16px -50px',
+  [MaskCell.flag]: '-34px -50px',
+  [MaskCell.question]: '-50px -50px',
+};
+
 function Field() {
-  const field = useCreateField(16);
+  const fieldSize = 16;
+  const dimension = new Array(fieldSize).fill(null);
+
+  const [isLose, setIsLose] = useState(false);
+  const [isWin, setIsWin] = useState(false);
+  const [field, setField] = useState<number[]>(() => useCreateField(fieldSize));
+  const [mask, setMask] = useState<MaskCell[]>(() => new Array(fieldSize * fieldSize).fill(MaskCell.hidden));
+
+  const cellClickHandler = (counter: number) => {
+    if (isLose || isWin) {
+      return null;
+    }
+
+    return null;
+  };
+
+  console.log(field);
+  console.log(mask);
 
   return (
-    <Flex flexWrap="wrap" maxW="272px" border="5px solid #939393">
-      {field.map((item) => (
-        <Button
-          cursor="pointer"
-          key={uuidv4()}
-          backgroundImage={imageSrc}
-          backgroundRepeat="no-repeat"
-          backgroundPosition="0px -50px"
-          outline="none"
-          border="none"
-          height="17px"
-          width="17px"
-        >
-          {item}
-        </Button>
+    <Box border="5px solid #939393">
+      {dimension.map((itemY, y) => (
+        <Flex flexWrap="wrap" key={uuidv4()} style={{ display: 'flex' }}>
+          {dimension.map((itemX, x) => (
+            <Button
+              cursor="pointer"
+              key={uuidv4()}
+              backgroundImage={sprite}
+              backgroundRepeat="no-repeat"
+              backgroundPosition={
+                mask[y * fieldSize + x] !== MaskCell.show
+                  ? MaskCellType[mask[y * fieldSize + x]]
+                  : field[y * fieldSize + x]
+              }
+              outline="none"
+              border="none"
+              height="17px"
+              width="17px"
+            >
+              {/* {mask[y * fieldSize + x] !== MaskCell.show
+                ? mapMaskToView[mask[y * fieldSize + x]]
+                : field[y * fieldSize + x]} */}
+            </Button>
+          ))}
+        </Flex>
       ))}
-    </Flex>
+    </Box>
   );
 }
 
